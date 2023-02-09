b6c05588c7b2e0a4e732ebef844a8825653c796a
Mohammad Muazam <m.muazam.99@gmail.com>
2023-02-09 15:55:03 +0530

    refactor

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("render empty screen", () => {
-  render(<App />);
-  expect(screen.getByText(/Username/i)).toBeInTheDocument();
-  const inputElement = screen.getByRole("textbox");
-  expect(inputElement).toBeInTheDocument();
-  expect(inputElement).toHaveValue("");
+test("render initial screen", () => {
+ render(<App />);
+ expect(screen.getByText(/Username/i)).toBeInTheDocument();
+ const inputElement = screen.getByRole("textbox");
+ expect(inputElement).toBeInTheDocument();
+ expect(inputElement).toHaveValue("");
 });
